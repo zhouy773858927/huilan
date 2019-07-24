b2b01b44c55149fd21f2d24040cb7c5058ad18c4
zhouy <773858927@qq.com>
2019-07-24 17:50:21 +0800

    2019.7.24更新：添加util-file_reader的excel配置模块;untils-HTMLTestRunner测试报告模块;utils-mail邮件发送模块

--- test/test_baidu.py
+++ test/test_baidu.py
@@ -1,34 +1,65 @@
+"""
+轻量级自动化测试框架
+2019.7.20：完成test_baidu测试流程
+2019.7.22更新：添加utils-file_reader的yml配置模块;utils-log日志模块
+2019.7.24更新：添加util-file_reader的excel配置模块;untils-HTMLTestRunner测试报告模块;utils-mail邮件发送模块
+"""
 import os
 import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from utils.config import Config,DRIVER_PATH
+from utils.config import Config,DRIVER_PATH,DATA_PATH,REPORT_PATH
 from utils.log import logger
+from utils.file_reader import ExcelReader
+from utils.HTMLTestRunner import HTMLTestRunner
+from utils.mail import Email
 import json
 
 class TestBaiDu(unittest.TestCase):
     URL = Config().get('URL')
+    excel = DATA_PATH + r'\baidu.xlsx'
     locator_kw = (By.ID, 'kw')
     locator_su = (By.ID, 'su')
     locator_result = (By.XPATH, '//*[@id="content_left"]')
-
-    def setUp(self):
+    #不要用unittest自带的setup，不然会在最开始多启动一次浏览器
+    def sub_setup(self):
         self.driver = webdriver.Chrome()
         self.driver.get(self.URL)
 
-    def tearDown(self):
+    def sub_tearDown(self):
         self.driver.quit()
 
     def test_search_0(self):
-        driver = self.driver
-        driver.find_element(*self.locator_kw).send_keys('selenium灰蓝')
-        driver.find_element(*self.locator_su).click()
-        time.sleep(2)
-        links = driver.find_element(*self.locator_result).text
-        logger.info(links)
+        datas = ExcelReader(self.excel).data
+        print(datas)
+        for d in datas:
+            with self.subTest(data=d):
+                self.sub_setup()
+                driver = self.driver
+                driver.find_element(*self.locator_kw).send_keys(d['search'])
+                driver.find_element(*self.locator_su).click()
+                time.sleep(2)
+                links = driver.find_element(*self.locator_result).text
+                logger.info(links)
+                self.sub_tearDown()
 
 
 
 if __name__ == '__main__':
-    unittest.main()
+    report = REPORT_PATH + '\\report.html'
+    print(report)
+    with open(report,'wb') as f:
+        runner = HTMLTestRunner(f, verbosity=2, title='轻量级测试框架-百度测试',description='百度用例')
+        #必须用unittest.main   否则无法生成报告
+        runner.run(TestBaiDu('test_search_0'))
+        e = Email(title='百度搜索测试报告',
+                  message='这是今天的测试报告，请查收！',
+                  receiver='773858927@qq.com',
+                  server='smtp.qq.com',
+                  sender='773858927@qq.com',
+                  password='hxmsxbafdhdqbceg',
+                  path=report
+                  )
+        e.send()
+        unittest.main(runner)
--- utils/file_reader.py
+++ utils/file_reader.py
@@ -1,5 +1,9 @@
+"""
+文件读取。YamlReader读取yaml文件，ExcelReader读取excel。
+"""
 import yaml
 import os
+from xlrd import open_workbook
 
 class YamlReader:
     def __init__(self,yamlf):
@@ -16,3 +20,70 @@ class YamlReader:
             with open(self.yamlf,'rb') as f:
                 self._data = list(yaml.safe_load_all(f))# load后是个generator，用list组织成列表
         return self._data
+
+class SheetTypeError(Exception):
+    pass
+
+class ExcelReader:
+    """
+     读取excel文件中的内容。返回list。
+     如：
+     excel中内容为：
+     | A  | B  | C  |
+     | A1 | B1 | C1 |
+     | A2 | B2 | C2 |
+     如果 print(ExcelReader(excel, title_line=True).data)，输出结果：
+     [{A: A1, B: B1, C:C1}, {A:A2, B:B2, C:C2}]
+     如果 print(ExcelReader(excel, title_line=False).data)，输出结果：
+     [[A,B,C], [A1,B1,C1], [A2,B2,C2]]
+     可以指定sheet，通过index或者name：
+     ExcelReader(excel, sheet=2)
+     ExcelReader(excel, sheet='BaiDuTest')
+
+    title_line参数，用来声明是否在excel表格里有标题行，如果有标题行，返回dict列表，否则返回list列表
+    如果title_line=True
+    [{"title1": "value1", "title2": "value2"}, {"title1": "value3", "title2": "value4"}]
+    如果title_line=False
+    [["title1", "title2"], ["value1", "value2"], ["value3", "value4"]]
+    """
+    def __init__(self,excel,sheet=0,title_line=True):
+        if  os.path.exists(excel):
+            self.excel = excel
+        else:
+            raise FileNotFoundError('文件不存在！')
+        self.sheet = sheet
+        self.title_line = title_line
+        self._data = list()
+
+    @property
+    def data(self):
+        if not self._data:
+            workbook = open_workbook(self.excel)
+            #判断excel表中sheet的属性并取值
+            if type(self.sheet) not in [int,str]:
+                raise SheetTypeError('Please pass in <type int> or <type str>,not{0}'.format(self.sheet))
+            elif type(self.sheet) == int:
+                s = workbook.sheet_by_index(self.sheet)
+            else:
+                s = workbook.sheet_by_name(self.sheet)
+
+            if self.title_line:
+                title = s.row_values(0)#首行为title
+                for col in range(1, s.nrows):
+                    #依次遍历其余行，与第一行组成dict字典，拼接到self._data中
+                    self._data.append(dict(zip(title,s.row_values(col))))
+            else:
+                for col in range(0,s.nrows):
+                    #遍历所有行，拼到self._data中
+                    self._data.append(s.row_values(col))
+        return self._data
+
+if __name__ == '__main__':
+    y = r'D:\huilan\config\config.yml'
+    reader_yml = YamlReader(y)
+    print(reader_yml.data)
+
+    e = r'D:\huilan\data\baidu.xlsx'
+    reader_excel = ExcelReader(e)
+    print(reader_excel.data)
+
